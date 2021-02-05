9f342281319d8be1ec94f72b741c0e3c6535769b
Daniel Visca <danielvisca96@gmail.com>
2021-02-04 22:08:37 -0500

    chrome extension broken but this is the closest instance I have saved to when it worked well

--- src/index.js
+++ src/index.js
@@ -1,66 +1,95 @@
-import axios from "axios";
+// Tutorial this is extension is loosely based on
+// https://github.com/onedebos/covtension/blob/master/src/index.js
 const log = console.log
+
+import axios from "axios";
+
+// API endpoint
+// const api = "https://cdbc305686aa.ngrok.io/analysis";
 const api = "https://know-your-news.herokuapp.com/analysis";
-// const errors = document.getElementsByClassName("errors");
-const results = document.getElementsByClassName("result-container")
-const loading = document.getElementsByClassName("loading");
-const sentiment = document.getElementsByClassName("sentiment");
-const topic = document.getElementsByClassName("topic");
-const bias = document.getElementsByClassName("bias");
-const factual_reporting = document.getElementsByClassName("factual-reporting");
-const mbfc_url = document.getElementsByClassName("mbfc-url");
 
+// Retrieve elements from html
+const errors = document.getElementsByClassName("errors")[0];
+const results = document.getElementsByClassName("result-container")[0];
+const loading = document.getElementsByClassName("loading")[0];
+const sentiment = document.getElementsByClassName("sentiment")[0];
+const topic = document.getElementsByClassName("topic")[0];
+const bias = document.getElementsByClassName("bias")[0];
+const source = document.getElementsByClassName("source")[0];
+const factual_reporting = document.getElementsByClassName("factual-reporting")[0];
+const mbfc_url = document.getElementsByClassName("mbfc-url")[0];
+const mbfc = document.getElementsByClassName("mbfc")[0];
+const title = document.getElementsByClassName("title")[0];
+const title_sentiment = document.getElementsByClassName("title-sentiment")[0];
 
-log(sentiment)
-log(topic)
-log(results)
-let current_url = "";
-// loading.style.display = "none";
-// errors.style.textContent = "";
-results[0].style.display = "none";
-
-// get the button that starts the analysis
-const btn = document.querySelector(".start-btn");
+// display nothing to begin with
+loading.style.display = "none";
+errors.style.textContent = "";
+results.style.display = "none";
+// mbfc.style.display = "";
 
 // declare a method to start the analysis
-const analyse_page = async => {
-  // loading.style.display = "block";
-  // errors.textContent = "";
-  // get url
+const analyse_page = () => {
+  loading.style.display = "block";
+  errors.style.textContent = "";
+
+  // Retrieve the current tab, this call is async
   chrome.tabs.query({ active: true, currentWindow: true}, function (tabs) {
-      console.log(tabs[0].url.toString())
-      current_url = tabs[0].url.toString();
-    })
-  // call api
-  try {
-    axios.post(api, {
-      url: current_url
-    })
-    .then((response) => {
-      console.log("response.data")
-      console.log(response.data)
-      // loading.style.display = "none";
-      sentiment[0].textContent = response.data.sentiment.text;
-      topic[0].textContent = response.data.topics;
-      bias[0].textContent = response.data.mbfc.Bias
-      factual_reporting[0].textContent = response.data.mbfc["Factual Reporting"]
-      mbfc_url[0].textContent = response.data.mbfc.URL
-      results[0].style.display = "block";
-    })
-    .catch(error => {
-      console.log(error.response)
-    })
-  } catch (error) {
-    // loading.style.display = "none";
-    // errors.textContent = "There was an error"
-    log("There is an error")
-  }
+    let current_url = tabs[0].url.toString();
+
+    // call api
+    try {
+      axios.post(api, {
+        url: current_url
+      })
+      .then((response) => {
+        // Response is successful
+        log("current_url")
+        log(current_url)
+        log("response")
+        log(response.data)
+        // Populate Article info
+        title.textContent = response.data.title;
+        title_sentiment.textContent = response.data.sentiment.title;
+        sentiment.textContent = response.data.sentiment.text;
+        topic.textContent = response.data.topics;
+        // media bias data is not null, populate mbfc info
+        // if (response.data.mbfc != null) {
+        source.textContent = response.data.mbfc.Source
+        bias.textContent = response.data.mbfc.Bias
+        mbfc_url.textContent = response.data.mbfc.URL
+        const facts = response.data.mbfc["Factual Reporting"]
+          // Factual Reporting is Null when the site is satire
+          //if (facts == ""){
+          //  factual_reporting.textContent = "Fake News"
+          //} else {
+        factual_reporting.textContent = facts
+          //}
+          // display mbfc div
+        // mbfc.style.display = "block";
+        // }
+        // mbfc is null, do not display div
+        // else {
+        //   mbfc.style.display = "";
+        // }
+        // hide loadimg, display results
+        loading.style.display = "none";
+        results.style.display = "block";
+      })
+      .catch(error => {
+        // Hide loading, display error
+        loading.style.display = "none";
+        errors.textContent = "Uh oh! There was an error with out server, Try again?"
+        console.log(error.response) 
+      })
+    } catch (error) {
+      // Hide loading, display error
+      loading.style.display = "none";
+      errors.textContent = "There was an API error"
+      log("There is an error")
+    }
+  })
 }
 
-// declare a function to handle button click
-const handleClick = async e => {
-  e.preventDefault();
-  analyse_page();
-}
+analyse_page();
 
-btn.addEventListener("click", e => handleClick(e))
