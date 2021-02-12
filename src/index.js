4d219fa0a7a14862b868904cb1c994fe27efd36b
Daniel Visca <danielvisca96@gmail.com>
2021-02-11 20:34:18 -0500

    feature implemented

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ const log = console.log
 import axios from "axios";
 
 // API endpoints
+const api = "https://ed57d60bf67b.ngrok.io/";
 // const api = "https://dev-know-your-news.herokuapp.com/";
-const api = "https://know-your-news.herokuapp.com/";
+// const api = "https://know-your-news.herokuapp.com/";
 const analysis_api = api + "analysis";
 const error_report_api = api + "error_report";
 // Retrieve elements from html
@@ -40,6 +41,12 @@ const similarity_score = document.getElementsByClassName("similarity-score")[0];
 // User Error reporting
 const report_error = document.getElementsByClassName("report-error")[0];
 const report_error_text = document.getElementsByClassName("report-error-text")[0];
+const check_boxes = document.getElementsByClassName("checkbox");
+const submit_error_report = document.getElementById("submit-error-report");
+
+// const article_error_btn = document.getElementById("article-error");
+// const mbfc_error_btn = document.getElementById("mbfc-error");
+// const related_news_error_btn = document.getElementById("related-news-error");
 
 // Elements displays (what elements can be seen by user to begin with)
 
@@ -50,6 +57,16 @@ results_container.style.display = "none";
 article_container.style.display = "none";
 mbfc_container.style.display = "none";
 related_news_container.style.display = "none";
+submit_error_report.style.display = "none";
+
+// Hide checkboxes for error reporting
+const hide_checkboxes = () => {
+  for (let i=0; i < check_boxes.length; i++) {
+    let box = check_boxes[i];
+    box.style.display = "none";
+  }
+}
+hide_checkboxes()
 
 // make global so error reporting has access to it
 let current_url;
@@ -116,18 +133,31 @@ const analyse_page = () => {
           } else {
             factual_reporting.textContent = facts
           }
-          // display mbfc div
+          // This news source has been mislabeled in the past and could be wrong in this situation
+          // However, this user could be on the correct news source for this labelling.
+          if ("reported_error" in response.data.mbfc) {
+            mbfc_container.classList.add('errors')
+            const p_tag = document.createElement('p')
+            const strong = document.createElement('strong')
+            const warning = document.createTextNode('Warning: This may be the wrong source') 
+            strong.appendChild(warning)
+            p_tag.appendChild(strong)
+            mbfc_container.prepend(p_tag);
+          }
           mbfc_container.style.display = "block";
         }
         // mbfc is null, do not display div
         else {
           mbfc_container.style.display = "none";
         }
+        log("Just above similar news")
         // NOTE: response.data.related_news.similarity_score could be undefined. Need to test if this will fail
         if (response.data.related_news != null || response.data.related_news.similarity_score != null) {
+          log("Inside similar news")
           similar_news_title.textContent = response.data.related_news.most_similar_title;
           similar_news_title.href = response.data.related_news.most_similar_url;
           similarity_score.textContent = response.data.related_news.similarity_score + '%';
+          log("done similar news")
           // display related news div
           related_news_container.style.display = "block";
         } else {
@@ -136,6 +166,7 @@ const analyse_page = () => {
 
         // Indicate that a a User marked this article as incorrect
         if ("reported_error" in response.data) {
+          log("error with specifc url")
           errors.textContent = "A User indicated that our analysis on this Article in incorrect";
           errors.style.display = "block";
         }
@@ -160,14 +191,41 @@ const analyse_page = () => {
   })
 }
 
-
+/*
+When the user clicks that there is an error, display check boxes.
+Wait until they select 'submit error report' then make API call with error info
+*/
 report_error.addEventListener('click', () => {
+  // Unhide checkboxes
+  for (let i=0; i < check_boxes.length; i++) {
+    let box = check_boxes[i];
+    box.style.display = "block";
+  }
+  report_error_text.innerHTML = "Select the boxes with errors, then submit.";
+  submit_error_report.style.display = "block";
+})
+
+
+function getSelectedCheckboxValues(name) {
+  const checkboxes = document.querySelectorAll(`input[name="${name}"]:checked`);
+  let values = [];
+  checkboxes.forEach((checkbox) => {
+      values.push(checkbox.value);
+  });
+  return values;
+}
+
+submit_error_report.addEventListener('click', () => {
+  log("Recorded Errors")
   try {
     axios.post(error_report_api, {
-      url: current_url
+      url: current_url,
+      errors: getSelectedCheckboxValues('error_box')
     })
     .then((response) => {
-      report_error_text.textContent = "We have been notifed, thank you!"
+      report_error_text.textContent = "We have been notifed, thank you!";
+      submit_error_report.style.display = "none";
+      hide_checkboxes()
     }) 
     .catch(error => {
       // NOTE: because I am only using the first index of class report, this inner HTML may not work. Needs to be tested
